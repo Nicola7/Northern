60200d17692fae037438590d8096ef17704b9799
Nicola Miranda <nsmirandan@unal.edu.co>
2016-11-15 18:09:48 -0500

    version con comentarios

--- northern.cpp
+++ northern.cpp
@@ -23,6 +23,7 @@ int main (void){
   std::cout<<h<<'\t'<<R<<'\t'<<Z<<'\n';
   double v=stormerv(Ri,Zi,Rpi,Zpi,h);
   double y=stormery(Ri,Zi,Rpi,Zpi,h);
+  /*todo lo que este arriba de este comentario es para calcular el valor de las condiciones iniciales y el "for" es para calcular los valores de la serie*/
   for (int ii=0;ii<=(n-2);++ii){
     double r,z,u,i;
     r=R;    z=Z;    u=v;    i=y;
@@ -34,6 +35,7 @@ int main (void){
   }
   return 0;
 }
+/*la funcion SDR calcula el valor de la segunda derivada de R dados uno valores R y z*/
 double SDR(double R,double z){
   double y=(-0.5);
   double r=std::hypot(R,z);
@@ -41,6 +43,7 @@ double SDR(double R,double z){
   t*=(((2*y)/(R*R))+((3*(R*R))/(std::pow(r,5)))-(1.0/(std::pow(r,3))));
   return t;
 }
+/*la funcion SDZ calcula el valor de la segunda derivada de z dados ciertos valores de R y z*/
 double SDZ (double R,double z){
   double y=(-0.5);
   double r=std::hypot(R,z);
@@ -48,17 +51,20 @@ double SDZ (double R,double z){
   t*=(((2*y)/R)+((R)/(std::pow(r,3))));
   return t;
 }
+/* La funcion stormerR calcula el valor de R para s+h donde s es el valor en el que se evaluo la R y z que se tienen coo argumentos de la funcion*/
 double stormerR(double R,double z,double v,double h){
   double n=((((SDR(R,z))*(h*0.5))+v)*h);
   n+=R;
   return n;
 }
+/*identico que la funcion anterior pero para z*/
 double stormerZ(double R,double z,double y,double h){
   double n=((((SDZ(R,z))*(h*0.5))+y)*h);
   n+=z;
   return n;
 
 }
+/*esta funcion stormerv calcula el valor de la primera derivada de R para s+h donde s es el valor que R y Z que se tienen como argumentos de la funcion*/
 double stormerv(double R,double z,double v,double y,double h){
   double n=stormerR(R,z,v,h);
   double t=stormerZ(R,z,y,h);
@@ -67,6 +73,7 @@ double stormerv(double R,double z,double v,double y,double h){
   n+=v;
   return n;
 }
+/*igual que la anterior funcion pero todo para z*/
 double stormery(double R,double z,double v,double y,double h){
   double n=stormerR(R,z,v,h);
   double t=stormerZ(R,z,y,h);
